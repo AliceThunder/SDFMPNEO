f27b855ad6114ba2f018aeb2ae2f7cd3fc3d1df0
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-04 12:20:43 +0800

    Expose thermal and flow physics helpers

--- src/sdfmpneo/physics/__init__.py
+++ src/sdfmpneo/physics/__init__.py
@@ -4,10 +4,24 @@ from .em import (
     sea_loss_consistency,
     solve_em_schur,
 )
+from .flow import (
+    magnetic_reynolds_number,
+    motional_emf_required,
+    positive_sst_state,
+    turbulent_thermal_conductivity,
+)
+from .thermal import conservative_mortar_matrix, rms_joule_power, thermal_jump
 
 __all__ = [
     "solve_em_schur",
     "reciprocity_defect",
     "minimum_dissipation_eigenvalue",
     "sea_loss_consistency",
+    "conservative_mortar_matrix",
+    "thermal_jump",
+    "rms_joule_power",
+    "positive_sst_state",
+    "turbulent_thermal_conductivity",
+    "magnetic_reynolds_number",
+    "motional_emf_required",
 ]
