586112d71df4aeb560c5e084010f827cf88cc8c7
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-04 13:02:55 +0800

    Expose matrix-free EM projection API

--- src/sdfmpneo/physics/__init__.py
+++ src/sdfmpneo/physics/__init__.py
@@ -1,8 +1,12 @@
 from .em import (
+    DenseEMActionAdapter,
     minimum_dissipation_eigenvalue,
+    project_matrix_free_em_system,
     reciprocity_defect,
     sea_loss_consistency,
+    solve_em_matrix_free,
     solve_em_schur,
+    solve_reduced_em_system,
 )
 from .flow import (
     magnetic_reynolds_number,
@@ -13,6 +17,10 @@ from .flow import (
 from .thermal import conservative_mortar_matrix, rms_joule_power, thermal_jump
 
 __all__ = [
+    "DenseEMActionAdapter",
+    "project_matrix_free_em_system",
+    "solve_reduced_em_system",
+    "solve_em_matrix_free",
     "solve_em_schur",
     "reciprocity_defect",
     "minimum_dissipation_eigenvalue",
