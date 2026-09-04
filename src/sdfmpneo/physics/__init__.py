355be183f73e512fe19a503f738067939fe271fc
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-04 12:14:45 +0800

    Expose physics layers

---
+++ src/sdfmpneo/physics/__init__.py
@@ -0,0 +1,13 @@
+from .em import (
+    minimum_dissipation_eigenvalue,
+    reciprocity_defect,
+    sea_loss_consistency,
+    solve_em_schur,
+)
+
+__all__ = [
+    "solve_em_schur",
+    "reciprocity_defect",
+    "minimum_dissipation_eigenvalue",
+    "sea_loss_consistency",
+]
