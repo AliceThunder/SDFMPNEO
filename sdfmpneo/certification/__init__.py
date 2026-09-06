81a997635ed50f9e5e1a1b6ec77769c24ff90460
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:26:16 +0800

    Export continuous affine EM domain certification interfaces

--- sdfmpneo/certification/__init__.py
+++ sdfmpneo/certification/__init__.py
@@ -1,3 +1,25 @@
-from .basic import ContractionCertificate, StateErrorCertificate, contraction_certificate, state_error_certificate
+from .basic import (
+    ContractionCertificate,
+    StateErrorCertificate,
+    contraction_certificate,
+    state_error_certificate,
+)
+from .em_domain import (
+    BoxResidualBound,
+    ContinuousEMResidualCertificate,
+    ParameterBox,
+    bound_affine_reduced_residual_on_box,
+    certify_affine_reduced_residual_domain,
+)
 
-__all__ = ["ContractionCertificate", "StateErrorCertificate", "contraction_certificate", "state_error_certificate"]
+__all__ = [
+    "ContractionCertificate",
+    "StateErrorCertificate",
+    "contraction_certificate",
+    "state_error_certificate",
+    "ParameterBox",
+    "BoxResidualBound",
+    "ContinuousEMResidualCertificate",
+    "bound_affine_reduced_residual_on_box",
+    "certify_affine_reduced_residual_domain",
+]
