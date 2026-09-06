e962fce71bfb420b6ea9971d11de22aa3450b9e9
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:34:09 +0800

    Export exact operating-parameter electrothermal residual interfaces

--- sdfmpneo/training/__init__.py
+++ sdfmpneo/training/__init__.py
@@ -5,11 +5,19 @@ from .growth import (
     TangentResidualGrower,
     product_candidates,
 )
+from .parametric_residual import (
+    AffineOperatingRHSMap,
+    ParametricElectroThermalResidual,
+    ParametricResidualSample,
+)
 from .residual import ElectroThermalResidual, ResidualSample
 
 __all__ = [
     "ElectroThermalResidual",
     "ResidualSample",
+    "AffineOperatingRHSMap",
+    "ParametricElectroThermalResidual",
+    "ParametricResidualSample",
     "AnalyticGrowthCandidate",
     "GrowthProposal",
     "GrowthScore",
