4059fbd0d58bf281ebe8ee5932f626af79541ec0
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:32:43 +0800

    Expose intrinsically parametric analytic evolution graph

--- sdfmpneo/__init__.py
+++ sdfmpneo/__init__.py
@@ -1,15 +1,22 @@
 """SDF-MPNEO executable core."""
 
-from .analytic import AnalyticEvolutionGraph, AnalyticSeries
+from .analytic import (
+    AnalyticEvolutionGraph,
+    AnalyticSeries,
+    ParametricAnalyticEvolutionGraph,
+    ParametricAnalyticSeries,
+)
 from .em import ParametricEMProblem, ReducedEMModel, ResidualGreedyEMReducer
 from .model import ExecutableSDFMPNEOModel, OnlinePrediction
 from .thermal import ThermalSpectralModel
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "AnalyticEvolutionGraph",
     "AnalyticSeries",
+    "ParametricAnalyticEvolutionGraph",
+    "ParametricAnalyticSeries",
     "ThermalSpectralModel",
     "ParametricEMProblem",
     "ReducedEMModel",
