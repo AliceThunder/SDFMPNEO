5510a00e7389b1e2502632af11b66eade768a1c6
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 13:52:01 +0800

    Export 3D compatible spatial assembly

--- sdfmpneo/em/__init__.py
+++ sdfmpneo/em/__init__.py
@@ -1,8 +1,18 @@
 from .compatible import CompatibleAphiDiscretization
+from .grid3d import (
+    RectilinearComplex3D,
+    SpatialAphiAssembly,
+    build_compatible_aphi_from_cells,
+    face_loop_source,
+)
 from .reduced import ParametricEMProblem, ReducedEMModel, ResidualGreedyEMReducer
 
 __all__ = [
     "CompatibleAphiDiscretization",
+    "RectilinearComplex3D",
+    "SpatialAphiAssembly",
+    "build_compatible_aphi_from_cells",
+    "face_loop_source",
     "ParametricEMProblem",
     "ReducedEMModel",
     "ResidualGreedyEMReducer",
