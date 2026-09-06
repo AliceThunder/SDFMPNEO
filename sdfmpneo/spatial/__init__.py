d276825bbf8fd0a1c58286c017fbc66cbcdf1a7e
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 21:14:57 +0800

    Export underwater WPT geometry chart and tagged mesh import

--- sdfmpneo/spatial/__init__.py
+++ sdfmpneo/spatial/__init__.py
@@ -1,8 +1,20 @@
 from .nedelec_weighted import assemble_weighted_nedelec_mass
 from .tetra3d import TetrahedralComplex3D, TetrahedralThermalAssembly
+from .uwpt_geometry import (
+    RigidPose,
+    SpiralCoilGeometry,
+    TaggedTetrahedralMesh,
+    UnderwaterWPTGeometry,
+    read_gmsh_v22_ascii,
+)
 
 __all__ = [
     "TetrahedralComplex3D",
     "TetrahedralThermalAssembly",
     "assemble_weighted_nedelec_mass",
+    "RigidPose",
+    "SpiralCoilGeometry",
+    "UnderwaterWPTGeometry",
+    "TaggedTetrahedralMesh",
+    "read_gmsh_v22_ascii",
 ]
