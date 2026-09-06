cfe5f2ac4dac66a4233329469e0cdcea255f1fd7
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:47:16 +0800

    Export exact weighted Nedelec tetrahedral integration

--- sdfmpneo/spatial/__init__.py
+++ sdfmpneo/spatial/__init__.py
@@ -1,3 +1,8 @@
+from .nedelec_weighted import assemble_weighted_nedelec_mass
 from .tetra3d import TetrahedralComplex3D, TetrahedralThermalAssembly
 
-__all__ = ["TetrahedralComplex3D", "TetrahedralThermalAssembly"]
+__all__ = [
+    "TetrahedralComplex3D",
+    "TetrahedralThermalAssembly",
+    "assemble_weighted_nedelec_mass",
+]
