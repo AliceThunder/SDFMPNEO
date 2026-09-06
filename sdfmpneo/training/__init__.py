9b19de2b15099c680043d18aeab9c0507ab8c9c0
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 13:38:42 +0800

    Export deterministic analytic growth interfaces

--- sdfmpneo/training/__init__.py
+++ sdfmpneo/training/__init__.py
@@ -1,3 +1,18 @@
+from .growth import (
+    AnalyticGrowthCandidate,
+    GrowthProposal,
+    GrowthScore,
+    TangentResidualGrower,
+    product_candidates,
+)
 from .residual import ElectroThermalResidual, ResidualSample
 
-__all__ = ["ElectroThermalResidual", "ResidualSample"]
+__all__ = [
+    "ElectroThermalResidual",
+    "ResidualSample",
+    "AnalyticGrowthCandidate",
+    "GrowthProposal",
+    "GrowthScore",
+    "TangentResidualGrower",
+    "product_candidates",
+]
