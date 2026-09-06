3adb7653107f966ae8c20dc935dec8d84d9b70ee
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 17:21:52 +0800

    Bump SDF-MPNEO to 0.8.0 for certified inexact Riesz actions

--- sdfmpneo/__init__.py
+++ sdfmpneo/__init__.py
@@ -22,7 +22,7 @@ from .model import (
 from .tetra_core import TetrahedralElectroThermalCore
 from .thermal import ThermalSpectralModel
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 __all__ = [
     "AnalyticEvolutionGraph",
