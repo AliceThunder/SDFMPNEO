8c93d435dc6bc04f0ab8249edfdd6ef057ce956f
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:16:49 +0800

    Update compatible EM regression for scaled reciprocal A-psi coordinates

--- tests/test_compatible_em.py
+++ tests/test_compatible_em.py
@@ -15,7 +15,7 @@ def make_discretization():
     ])
     source = np.array([1.0, 0.2])
     omega = 3.0
-    metric = np.eye(2)
+    metric_placeholder = np.eye(2)
     W0 = np.array([
         np.diag([0.6, 0.1]),
         np.diag([0.2, 0.5]),
@@ -23,10 +23,22 @@ def make_discretization():
     Wstate = np.zeros((2, 2, 2, 2))
     Wstate[0, 0] = np.diag([0.02, 0.01])
     Wstate[1, 1] = np.diag([0.01, 0.03])
-    return CompatibleAphiDiscretization(C, G, R, Nu, S0, S_state, source, omega, metric, W0, Wstate)
+    return CompatibleAphiDiscretization(
+        C,
+        G,
+        R,
+        Nu,
+        S0,
+        S_state,
+        source,
+        omega,
+        metric_placeholder,
+        W0,
+        Wstate,
+    )
 
 
-def test_compatible_aphi_assembly_blocks_and_topology():
+def test_compatible_aphi_assembly_blocks_topology_and_reciprocity():
     disc = make_discretization()
     problem = disc.to_parametric_problem()
     assert np.allclose(disc.curl @ disc.grad_c, 0.0)
@@ -35,11 +47,20 @@ def test_compatible_aphi_assembly_blocks_and_topology():
     assert problem.H_loss.shape == (2, 2, 2)
     assert problem.H_loss_state.shape == (2, 2, 2, 2)
 
-    C, R, S = disc.curl, disc.a_basis, disc.conductivity0
+    C, R, G, S = disc.curl, disc.a_basis, disc.grad_c, disc.conductivity0
     K_A = R.T @ (C.T @ disc.reluctivity_hodge @ C) @ R
-    expected_tl = K_A + 1j * disc.omega * (R.T @ S @ R)
+    jw = 1j * disc.omega
+    expected_tl = K_A + jw * (R.T @ S @ R)
+    expected_tr = jw * (R.T @ S @ G)
+    expected_br = jw * (G.T @ S @ G)
+
     assert np.allclose(problem.A0[:1, :1], expected_tl)
-    assert np.allclose(problem.A0[:1, 1:], R.T @ S @ disc.grad_c)
+    assert np.allclose(problem.A0[:1, 1:], expected_tr)
+    assert np.allclose(problem.A0[1:, :1], expected_tr.T)
+    assert np.allclose(problem.A0[1:, 1:], expected_br)
+    assert np.allclose(problem.A0, problem.A0.T)
+    for Ak in problem.A_state:
+        assert np.allclose(Ak, Ak.T)
 
 
 def test_compatible_problem_reduces_and_loss_jacobian_matches_difference():
