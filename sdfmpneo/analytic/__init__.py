2081f1bb7b062c8f86852c8bb86224fce060be3f
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-06 14:32:12 +0800

    Export intrinsically parametric analytic neural graph

--- sdfmpneo/analytic/__init__.py
+++ sdfmpneo/analytic/__init__.py
@@ -1,6 +1,13 @@
 from .algebra import AnalyticSeries, DeferredResponseSeries, ResponseTerm, solve_response_series
 from .compiler import CompiledAnalyticKernel
 from .graph import AnalyticEvolutionGraph, CompiledAnalyticGraph
+from .parametric import (
+    CompiledParametricAnalyticGraph,
+    ParametricAnalyticEvolutionGraph,
+    ParametricAnalyticSeries,
+    ParametricResponseNode,
+    solve_parametric_response_series,
+)
 from .realization import AnalyticRealization, CompiledRealizationGraph
 
 __all__ = [
@@ -13,4 +20,9 @@ __all__ = [
     "CompiledAnalyticKernel",
     "AnalyticRealization",
     "CompiledRealizationGraph",
+    "ParametricAnalyticSeries",
+    "ParametricResponseNode",
+    "CompiledParametricAnalyticGraph",
+    "ParametricAnalyticEvolutionGraph",
+    "solve_parametric_response_series",
 ]
