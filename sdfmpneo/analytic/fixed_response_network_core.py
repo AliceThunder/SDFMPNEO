f2ba31b7272830cb9461bb14587498952341287b
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-11 06:42:15 -0400

    Use inactive-layer shortcut in response network core

--- sdfmpneo/analytic/fixed_response_network_core.py
+++ sdfmpneo/analytic/fixed_response_network_core.py
@@ -1,13 +1,14 @@
 from .fixed_response_init import _NetworkInitMixin
 from .fixed_response_structure import _NetworkStructureMixin
 from .fixed_response_fast import _DepthOneFastMixin
+from .fixed_response_activation import _LayerActivationMixin
 from .fixed_response_generic import _GenericAnalyticMixin
 from .fixed_response_metadata import _NetworkMetadataMixin
 
 
 class FixedAnalyticResponseNetwork(
     _NetworkInitMixin, _NetworkStructureMixin, _DepthOneFastMixin,
-    _GenericAnalyticMixin, _NetworkMetadataMixin,
+    _LayerActivationMixin, _GenericAnalyticMixin, _NetworkMetadataMixin,
 ):
     """Finite-horizon multilayer analytic response network with low-rank coupling."""
 
