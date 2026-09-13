cbc4ee0425104905ca683f91b9114a94c493ed28
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-13 07:06:20 -0400

    Fix package exports after sparse neural solver rewrite

--- sdfmpneo/__init__.py
+++ sdfmpneo/__init__.py
@@ -1,4 +1,4 @@
-"""SDF-MPNEO unified full-space residual-corrected neural electrothermal solver."""
+"""SDF-MPNEO unified full-space neural electrothermal solver."""
 
 from .unified_background import BackgroundContext, FixedMultiscaleBackground, stretched_axis
 from .unified_dataset import MaxwellResidualDataset, generate_residual_dataset
@@ -7,23 +7,24 @@ from .unified_maxwell import MaxwellSolveReport, NeuralMaxwellAccelerator
 from .unified_model import ARCHITECTURE, UnifiedNeuralElectroThermalModel, UnifiedPrediction, UnifiedSteadyState
 from .unified_neural_operator import (
     EdgeMultiscaleConfig,
+    OperatorGraph,
     build_edge_residual_operator,
-    edge_group_ids,
     edge_static_features,
+    operator_feature_statistics,
     residual_features,
 )
 from .unified_thermal import ThermalBasisReport, build_thermal_basis
 from .unified_trainer import MaxwellTrainingConfig, MaxwellTrainingReport, train_maxwell_accelerator
 
-__version__ = "0.12.0"
+__version__ = "0.12.1"
 
 __all__ = [
     "ARCHITECTURE", "BackgroundContext", "CoilGeometry", "EdgeMultiscaleConfig",
     "FixedMultiscaleBackground", "MaxwellResidualDataset", "MaxwellSolveReport",
     "MaxwellTrainingConfig", "MaxwellTrainingReport", "NeuralMaxwellAccelerator",
-    "PackageGeometry", "Pose", "ThermalBasisReport", "UnifiedNeuralElectroThermalModel",
-    "UnifiedPrediction", "UnifiedSteadyState", "UnifiedUWPTGeometry",
-    "build_edge_residual_operator", "build_thermal_basis", "edge_group_ids",
-    "edge_static_features", "generate_residual_dataset", "residual_features",
-    "sample_geometry", "stretched_axis", "train_maxwell_accelerator",
+    "OperatorGraph", "PackageGeometry", "Pose", "ThermalBasisReport",
+    "UnifiedNeuralElectroThermalModel", "UnifiedPrediction", "UnifiedSteadyState",
+    "UnifiedUWPTGeometry", "build_edge_residual_operator", "build_thermal_basis",
+    "edge_static_features", "generate_residual_dataset", "operator_feature_statistics",
+    "residual_features", "sample_geometry", "stretched_axis", "train_maxwell_accelerator",
 ]
