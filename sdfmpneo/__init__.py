81fa92e5b54e4c0f0970004fcd111a61dc5e1c38
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-13 08:17:53 -0400

    Export multiscale neural operator API

--- sdfmpneo/__init__.py
+++ sdfmpneo/__init__.py
@@ -1,4 +1,4 @@
-"""SDF-MPNEO unified full-space neural electrothermal solver."""
+"""SDF-MPNEO unified multiscale full-space neural electrothermal solver."""
 
 from .unified_background import BackgroundContext, FixedMultiscaleBackground, stretched_axis
 from .unified_dataset import MaxwellResidualDataset, generate_residual_dataset
@@ -9,6 +9,7 @@ from .unified_neural_operator import (
     EdgeMultiscaleConfig,
     OperatorGraph,
     build_edge_residual_operator,
+    edge_multiscale_group_ids,
     edge_static_features,
     operator_feature_statistics,
     residual_features,
@@ -16,7 +17,7 @@ from .unified_neural_operator import (
 from .unified_thermal import ThermalBasisReport, build_thermal_basis
 from .unified_trainer import MaxwellTrainingConfig, MaxwellTrainingReport, train_maxwell_accelerator
 
-__version__ = "0.12.1"
+__version__ = "0.13.0"
 
 __all__ = [
     "ARCHITECTURE", "BackgroundContext", "CoilGeometry", "EdgeMultiscaleConfig",
@@ -25,6 +26,7 @@ __all__ = [
     "OperatorGraph", "PackageGeometry", "Pose", "ThermalBasisReport",
     "UnifiedNeuralElectroThermalModel", "UnifiedPrediction", "UnifiedSteadyState",
     "UnifiedUWPTGeometry", "build_edge_residual_operator", "build_thermal_basis",
-    "edge_static_features", "generate_residual_dataset", "operator_feature_statistics",
-    "residual_features", "sample_geometry", "stretched_axis", "train_maxwell_accelerator",
+    "edge_multiscale_group_ids", "edge_static_features", "generate_residual_dataset",
+    "operator_feature_statistics", "residual_features", "sample_geometry", "stretched_axis",
+    "train_maxwell_accelerator",
 ]
