ff772f9d071dbb9f78c87064b2b129082c091e8d
Zhu Chunyang <98133082+AliceThunder@users.noreply.github.com>
2026-09-21 04:35:12 -0400

    Exclude spatial mapping arrays from self-correction audit metadata

--- sdfmpneo/unified_self_correction_audit.py
+++ sdfmpneo/unified_self_correction_audit.py
@@ -185,7 +185,7 @@ def audit_local_self_correction(background, geometries, monitor=None):
                         "modal_h",
                         "localized_modal_h",
                         "localized_heat_cells",
-                        "local_background",
+                        "local_cell_centers",
                     )
                 },
                 "fine": {
